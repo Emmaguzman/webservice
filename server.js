bb6e56643705bd7746b90ac782431fc8e017e0ca
Emmanuel <emmanuel-a-guzman@hotmail.com>
2020-04-02 17:49:16 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+//XPRESS HBS ENGINE
+hbs.registerPartials(__dirname + "/views/parciales/");
+require('./hbs/helpers');
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        nombre: 'emmanuel'
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+// app.get('/', (req, res) => {
+//     let salida = {
+//         nombre: 'Emmanuel',
+//         edad: 32,
+//         url: req.url
+//     }
+//     res.send(salida)
+// });
+app.listen(3000, () => {
+    console.log("escuchando el puertazo 3000");
+});
